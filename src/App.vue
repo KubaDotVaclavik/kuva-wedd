d0460119f545b4a9eb8260203945275f8ff8ffdc
kvaclavik <kuba.vaclavik@gmail.com>
2022-12-20 23:14:28 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import backgroundImg from "./assets/bkg-small.png";
+</script>
+
+<template>
+  <div
+    class="relative shadow-md h-[690px] w-[430px] bg-bottom bg-fit bg-no-repeat"
+    :style="{ backgroundImage: `url(${backgroundImg})` }"
+  >
+    <div class="bg-gradient absolute w-full h-full">
+      <h1 class="text-4xl text-center font-mirage mt-20">Šárka + Jakub</h1>
+      <h2 class="text-4xl text-center font-astralaga mt-20">Šárka + Jakub</h2>
+    </div>
+  </div>
+</template>
